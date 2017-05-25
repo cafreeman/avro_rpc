526787ba97d225d030bf7d3211f419c0eb1f69f8
Chris Freeman <cfreeman.code@gmail.com>
2017-05-25 15:43:16 -0500

    Clean up config format

--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,7 @@
 use Mix.Config
 
-config :avro_rpc, hostname: "localhost"
-config :avro_rpc, port: 9015
-config :avro_rpc, protocol: "./test/data/avro.json"
+
+config :avro_rpc,
+   hostname: "localhost",
+   port: 9015,
+   protocol: "./test/data/avro.json"
