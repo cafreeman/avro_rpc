8438a4d039c96786722483a45163faf17c8d5052
Chris Freeman <cfreeman.code@gmail.com>
2017-05-31 12:16:02 -0500

    Add package details for Hex

--- mix.exs
+++ mix.exs
@@ -1,13 +1,43 @@
 defmodule AvroRPC.Mixfile do
+  @moduledoc false
   use Mix.Project
 
   def project do
-    [app: :avro_rpc,
-     version: "0.1.0",
-     elixir: "~> 1.4",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps()]
+    [
+      app: :avro_rpc,
+      version: "0.1.0",
+      description: description(),
+      package: package(),
+      deps: deps(),
+      elixir: "~> 1.4",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+
+      # Docs
+      name: "AvroRPC",
+      source_url: "https://github.com/cafreeman/avro_rpc",
+      docs: [
+        main: "AvroRPC.Client",
+        extras: ["README.md"]
+      ]
+    ]
+  end
+
+  defp description do
+    """
+    `AvroRPC` is an Elixir library for making RPC calls using the [Avro protocol specification](http://avro.apache.org/docs/1.7.7/spec.html). Currently, `AvroRPC` wraps functionality from both the [eavro](https://github.com/SIfoxDevTeam/eavro) and [ex_avro](https://github.com/avvo/ex_avro) libraries and provides a unified client implementation for managing connections and calling methods across multiple `AvroRPC` services.
+    """
+  end
+
+  defp package do
+    [
+      name: :avro_rpc,
+      files: ["lib", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      maintainers: ["Chris Freeman"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/cafreeman/avro_rpc"}
+    ]
+
   end
 
   # Configuration for the OTP application
@@ -38,6 +68,9 @@ defmodule AvroRPC.Mixfile do
     [
       {:eavro, git: "https://github.com/sifoxdevteam/eavro.git", tag: "v0.0.3"},
       {:ex_avro, "~> 0.1.0"},
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
     ]
   end
 end
